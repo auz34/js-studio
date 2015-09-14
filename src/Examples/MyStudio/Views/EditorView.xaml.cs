50a48e732a3743a9d9554c6436eb512b37b1b977
auz34 <andrewzavgorodniy@gmail.com>
2015-09-13 22:25:05 -0700

    Next UI blocks

---
+++ src/Examples/MyStudio/Views/EditorView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MyStudio.Views
+{
+    /// <summary>
+    /// Interaction logic for EditorView.xaml
+    /// </summary>
+    public partial class EditorView
+    {
+        public EditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
