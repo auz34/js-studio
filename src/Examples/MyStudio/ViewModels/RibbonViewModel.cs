6f9abd7bfbb7f031d7d7f0a804fc3e4db1efc4be
auz34 <andrewzavgorodniy@gmail.com>
2015-09-21 21:11:26 -0700

    Add recently used items data

--- src/Examples/MyStudio/ViewModels/RibbonViewModel.cs
+++ src/Examples/MyStudio/ViewModels/RibbonViewModel.cs
@@ -1,10 +1,15 @@
 ﻿namespace MyStudio.ViewModels
 {
+    using System.Collections.Generic;
+
     using Catel;
     using Catel.MVVM;
 
     using MyStudio.Models;
     using MyStudio.Services;
+
+    using Orchestra.Models;
+    using Orchestra.Services;
 
     public class RibbonViewModel : ViewModelBase
     {
@@ -12,14 +17,22 @@
 
         private ICommandsService commandsService;
 
+        private IRecentlyUsedItemsService recentlyUsedItemsService;
+
         public RibbonViewModel(StudioStateModel model,
-            ICommandsService commandsService)
+            ICommandsService commandsService,
+            IRecentlyUsedItemsService recentlyUsedItemsService)
         {
             Argument.IsNotNull(() => model);
             Argument.IsNotNull(() => commandsService);
+            Argument.IsNotNull(() => recentlyUsedItemsService);
 
             this.model = model;
             this.commandsService = commandsService;
+            this.recentlyUsedItemsService = recentlyUsedItemsService;
+
+            this.RecentlyUsedItems = new List<RecentlyUsedItem>(this.recentlyUsedItemsService.Items);
+            this.PinnedItems = new List<RecentlyUsedItem>(this.recentlyUsedItemsService.PinnedItems);
         }
 
 
@@ -46,5 +59,9 @@
                 return this.commandsService != null ? this.commandsService.RedoCommand : null;
             }
         }
+
+        public List<RecentlyUsedItem> RecentlyUsedItems { get; private set; }
+
+        public List<RecentlyUsedItem> PinnedItems { get; private set; }
     }
 }
